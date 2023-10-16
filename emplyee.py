d349b5d22c10c3037e2839ffcd6d44fab52b94b3
mailinglistid <mailinglistid@gmail.com>
2023-10-16 08:36:21 +0530

    added the code in the file

---
+++ emplyee.py
@@ -0,0 +1,11 @@
+# Get employee information
+first_name = input("Enter employee's first name: ")
+last_name = input("Enter employee's last name: ")
+age = input("Enter employee's age: ")
+
+# Print employee details
+print("\nEmployee Details:")
+print("First Name: " + first_name)
+print("Last Name: " + last_name)
+print("Age: " + age)
+
